8c30aeba1c04978226607c7e000a8c9126babbf0
Melih DİNÇER <m_dincer41@hotmail.com>
2023-11-23 16:58:48 +0300

    Entitylerin oluşturulması.

---
+++ SignalR.EntityLayer/Entities/Discount.cs
@@ -0,0 +1,11 @@
+﻿namespace SignalR.EntityLayer.Entities
+{
+    public class Discount
+    {
+        public int DiscountID { get; set; }
+        public string Title { get; set; }
+        public string Amount { get; set; } //İndirim oranı
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ SignalR.EntityLayer/Entities/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace SignalR.EntityLayer.Entities
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public decimal Fiyat { get; set; }
+        public string ImageUrl { get; set; }    
+        public bool ProductStatus { get; set; }    
+
+    }
+}
