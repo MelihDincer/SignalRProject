58c26f896357d7cd0df04fcdd93ebfbccd001557
Melih DİNÇER <m_dincer41@hotmail.com>
2023-11-30 15:49:49 +0300

    DataAccess Katmanının tamamlanması.

---
+++ SignalR.DataAccessLayer/EntityFramework/EfContactDal.cs
@@ -0,0 +1,16 @@
+﻿using SignalR.DataAccessLayer.Abstract;
+using SignalR.DataAccessLayer.Concrete;
+using SignalR.DataAccessLayer.Repositories;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalR.DataAccessLayer.EntityFramework
+{
+    public class EfContactDal : GenericRepository<Contact>, IContactDal
+    {
+        //Context sınıfında GenericRepository sınıfında bir constructor oluşturmuşturduk. O zaman burada GenericRepository'den miras alıyorsak bu sınıfta da bir constructor oluşturup orada context sınıfını base aldığımız gibi burada da bunu uygulamalıyız. 
+        //Yukarıdaki sebepten dolayı aşağıdaki constructor sınıfı oluşturulmuştur.
+        public EfContactDal(SignalRContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ SignalR.DataAccessLayer/EntityFramework/EfProductDal.cs
@@ -0,0 +1,16 @@
+﻿using SignalR.DataAccessLayer.Abstract;
+using SignalR.DataAccessLayer.Concrete;
+using SignalR.DataAccessLayer.Repositories;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalR.DataAccessLayer.EntityFramework
+{
+    public class EfProductDal : GenericRepository<Product>, IProductDal
+    {
+        //Context sınıfında GenericRepository sınıfında bir constructor oluşturmuşturduk. O zaman burada GenericRepository'den miras alıyorsak bu sınıfta da bir constructor oluşturup orada context sınıfını base aldığımız gibi burada da bunu uygulamalıyız. 
+        //Yukarıdaki sebepten dolayı aşağıdaki constructor sınıfı oluşturulmuştur.
+        public EfProductDal(SignalRContext context) : base(context)
+        {
+        }
+    }
+}
