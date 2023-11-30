476b0422fb5bba617e26f170aee0f554eda18e11
Melih DİNÇER <m_dincer41@hotmail.com>
2023-11-30 15:34:15 +0300

    Context sınıfının oluşturulması.

---
+++ SignalR.DataAccessLayer/Concrete/SignalRContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SignalR.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.DataAccessLayer.Concrete
+{
+    public class SignalRContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-PBE5IS4\\SQLEXPRESS; initial Catalog=SignalRDb; integrated Security=true");
+        }
+        
+        DbSet<About> Abouts { get; set; }
+        DbSet<Booking> Bookings { get; set; }
+        DbSet<Category> Categories { get; set; }
+        DbSet<Contact> Contacts { get; set; }
+        DbSet<Discount> Discounts { get; set; }
+        DbSet<Feature> Features { get; set; }
+        DbSet<Product> Products { get; set; }
+        DbSet<SocialMedia> SocialMedias { get; set; }
+        DbSet<Testimonial> Testimonials { get; set; }
+    }
+}
