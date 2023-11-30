5c79b75960f253488c6a2deeecf14f76e2e7ae1c
Melih DİNÇER <m_dincer41@hotmail.com>
2023-11-30 15:32:35 +0300

    Revize.

--- SignalR.EntityLayer/Entities/Product.cs
+++ SignalR.EntityLayer/Entities/Product.cs
@@ -5,7 +5,7 @@
         public int ProductID { get; set; }
         public string ProductName { get; set; }
         public string Description { get; set; }
-        public decimal Fiyat { get; set; }
+        public decimal Price { get; set; }
         public string ImageUrl { get; set; }    
         public bool ProductStatus { get; set; }    
 
