74c8192fdf750b88ea4acec36816f553532fb2c1
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-01 10:25:42 +0300

    ContactDto sınıflarının oluşturulması.

---
+++ SignalR.DtoLayer/ContactDto/GetContactDto.cs
@@ -0,0 +1,11 @@
+﻿namespace SignalR.DtoLayer.ContactDto
+{
+    public class GetContactDto
+    {
+        public int ContactID { get; set; }
+        public string Location { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Mail { get; set; }
+        public string FooterDescription { get; set; }
+    }
+}
