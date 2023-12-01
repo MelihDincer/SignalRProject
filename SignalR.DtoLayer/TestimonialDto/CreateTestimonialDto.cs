89fff2b128660ad2ba565f15fcc7aa29573f8a9a
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-01 10:26:17 +0300

    TestimonialDto sınıflarının oluşturulması.

---
+++ SignalR.DtoLayer/TestimonialDto/CreateTestimonialDto.cs
@@ -0,0 +1,11 @@
+﻿namespace SignalR.DtoLayer.TestimonialDto
+{
+    public class CreateTestimonialDto
+    {
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Comment { get; set; }
+        public string ImageUrl { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ SignalR.DtoLayer/TestimonialDto/ResultTestimonialDto.cs
@@ -0,0 +1,12 @@
+﻿namespace SignalR.DtoLayer.TestimonialDto
+{
+    public class ResultTestimonialDto
+    {
+        public int TestimonialID { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Comment { get; set; }
+        public string ImageUrl { get; set; }
+        public string Status { get; set; }
+    }
+}
