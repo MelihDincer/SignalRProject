6078ce11bdb00ce8702db56dc196fb2f780399e8
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-01 10:25:15 +0300

    AboutDto sınıflarının oluşturulması.

---
+++ SignalR.DtoLayer/AboutDto/CreateAboutDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SignalR.DtoLayer.AboutDto
+{
+    public class CreateAboutDto
+    {
+        public string ImageUrl { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
