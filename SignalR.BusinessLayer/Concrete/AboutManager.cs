cf0ee3e20c4053e0a994de63111186c3d475468b
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-01 10:24:20 +0300

    Business katmanı manager sınıflarının oluşturulması.

---
+++ SignalR.BusinessLayer/Concrete/AboutManager.cs
@@ -0,0 +1,41 @@
+﻿using SignalR.BusinessLayer.Abstract;
+using SignalR.DataAccessLayer.Abstract;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalR.BusinessLayer.Concrete
+{
+    public class AboutManager : IAboutService
+    {
+        private readonly IAboutDal _aboutDal;
+
+        public AboutManager(IAboutDal aboutDal)
+        {
+            _aboutDal = aboutDal;
+        }
+
+        public void TAdd(About entity)
+        {
+            _aboutDal.Add(entity);
+        }
+
+        public void TDelete(About entity)
+        {
+            _aboutDal.Delete(entity);
+        }
+
+        public About TGetByID(int id)
+        {
+            return _aboutDal.GetByID(id);
+        }
+
+        public List<About> TGetListAll()
+        {
+            return _aboutDal.GetListAll();
+        }
+
+        public void TUpdate(About entity)
+        {
+            _aboutDal.Update(entity);
+        }
+    }
+}
---
+++ SignalR.BusinessLayer/Concrete/TestimonialManager.cs
@@ -0,0 +1,41 @@
+﻿using SignalR.BusinessLayer.Abstract;
+using SignalR.DataAccessLayer.Abstract;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalR.BusinessLayer.Concrete
+{
+    public class TestimonialManager : ITestimonialService
+    {
+        private readonly ITestimonialDal _testimonialDal;
+
+        public TestimonialManager(ITestimonialDal testimonialDal)
+        {
+            _testimonialDal = testimonialDal;
+        }
+
+        public void TAdd(Testimonial entity)
+        {
+            _testimonialDal.Add(entity);    
+        }
+
+        public void TDelete(Testimonial entity)
+        {
+            _testimonialDal.Delete(entity);
+        }
+
+        public Testimonial TGetByID(int id)
+        {
+            return _testimonialDal.GetByID(id);
+        }
+
+        public List<Testimonial> TGetListAll()
+        {
+            return _testimonialDal.GetListAll();
+        }
+
+        public void TUpdate(Testimonial entity)
+        {
+            _testimonialDal.Update(entity);
+        }
+    }
+}
