1e269389397c87201b405091fe1fd04da87df9da
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-01 10:25:49 +0300

    DiscountDto sınıflarının oluşturulması.

---
+++ SignalR.DtoLayer/DiscountDto/GetDiscountDto.cs
@@ -0,0 +1,11 @@
+﻿namespace SignalR.DtoLayer.DiscountDto
+{
+    public class GetDiscountDto
+    {
+        public int DiscountID { get; set; }
+        public string Title { get; set; }
+        public string Amount { get; set; } //İndirim oranı
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
