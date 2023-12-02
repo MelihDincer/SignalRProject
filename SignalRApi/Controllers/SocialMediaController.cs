11b3921b270959f2540fea59b80b77c682ef83d6
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-02 10:21:27 +0300

    SocialMediaController'ın oluşturulması ve API kısmının tamamlanması.

---
+++ SignalRApi/Controllers/SocialMediaController.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.SocialMediaDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SocialMediaController : ControllerBase
+    {
+        private readonly ISocialMediaService _socialMediaService;
+        private readonly IMapper _mapper;
+
+        public SocialMediaController(ISocialMediaService socialMediaService, IMapper mapper)
+        {
+            _socialMediaService = socialMediaService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult SocialMediaList()
+        {
+            var value = _mapper.Map<List<SocialMedia>>(_socialMediaService.TGetListAll());
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public IActionResult CreateSocialMedia(CreateSocialMediaDto createSocialMediaDto)
+        {
+            _socialMediaService.TAdd(new SocialMedia()
+            {
+                Title = createSocialMediaDto.Title,
+                Url = createSocialMediaDto.Url,
+                Icon = createSocialMediaDto.Icon
+            });
+            return Ok("Sosyal Medya Bilgisi Eklendi");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteSocialMedia(int id)
+        {
+            var value = _socialMediaService.TGetByID(id);
+            _socialMediaService.TDelete(value);
+            return Ok("Sosyal Medya Bilgisi Silindi");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateSocialMedia(UpdateSocialMediaDto updateSocialMediaDto)
+        {
+            _socialMediaService.TUpdate(new SocialMedia()
+            {
+                SocialMediaID = updateSocialMediaDto.SocialMediaID,
+                Title = updateSocialMediaDto.Title,
+                Url = updateSocialMediaDto.Url,
+                Icon = updateSocialMediaDto.Icon
+            });
+            return Ok("Sosyal Medya Bilgisi Güncellendi");
+        }
+    }
+}
