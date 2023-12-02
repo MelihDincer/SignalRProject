cc4969b6aa89bd78e5bfd24325d53097c146e782
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-02 10:22:28 +0300

    İlişki için eklenen dto sınıfının (ResultProductWithCategory) mapleme işleminin yapılması.

--- SignalRApi/Mapping/ProductMapping.cs
+++ SignalRApi/Mapping/ProductMapping.cs
@@ -12,6 +12,7 @@ namespace SignalRApi.Mapping
             CreateMap<Product, CreateProductDto>().ReverseMap();
             CreateMap<Product, UpdateProductDto>().ReverseMap();
             CreateMap<Product, GetProductDto>().ReverseMap();
+            CreateMap<Product, ResultProductWithCategory>().ReverseMap();
         }
     }
 }
