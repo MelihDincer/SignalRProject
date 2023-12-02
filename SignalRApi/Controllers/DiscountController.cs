98bf5c3108937cf1fe840a86c0b0dfb5b9b62ea5
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-02 10:21:08 +0300

    DiscountController'ın oluşturulması ve API kısmının tamamlanması.

---
+++ SignalRApi/Controllers/DiscountController.cs
@@ -0,0 +1,72 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.DiscountDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiscountController : ControllerBase
+    {
+        private readonly IDiscountService _discountService;
+        private readonly IMapper _mapper;
+
+        public DiscountController(IDiscountService discountService, IMapper mapper)
+        {
+            _discountService = discountService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult DiscountList()
+        {
+            var values = _mapper.Map<List<ResultDiscountDto>>(_discountService.TGetListAll());
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateDiscount(CreateDiscountDto createDiscountDto)
+        {
+            _discountService.TAdd(new Discount()
+            {
+                Title = createDiscountDto.Title,
+                Amount = createDiscountDto.Amount,
+                Description = createDiscountDto.Description,
+                ImageUrl = createDiscountDto.ImageUrl,
+            });
+            return Ok("İndirim Bilgisi Eklendi");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteDiscount(int id)
+        {
+            var value = _discountService.TGetByID(id);
+            _discountService.TDelete(value);
+            return Ok("İndirim Bilgisi Silindi");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateDiscount(UpdateDiscountDto updateDiscountDto)
+        {
+            _discountService.TUpdate(new Discount()
+            {
+                DiscountID = updateDiscountDto.DiscountID,
+                Title = updateDiscountDto.Title,
+                Amount = updateDiscountDto.Amount,
+                Description = updateDiscountDto.Description,
+                ImageUrl = updateDiscountDto.ImageUrl,
+            });
+            return Ok("İndirim Bilgisi Güncellendi");
+        }
+
+        [HttpGet("GetDiscount")]
+        public IActionResult GetDiscount(int id)
+        {
+            var value = _discountService.TGetByID(id);
+            return Ok(value);
+        }
+    }
+}
