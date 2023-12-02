558d95d1789ef3d023eb640f4d421661df4ae0cd
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-02 10:21:19 +0300

    ProductController'ın oluşturulması ve API kısmının tamamlanması.

---
+++ SignalRApi/Controllers/ProductController.cs
@@ -0,0 +1,93 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DataAccessLayer.Concrete;
+using SignalR.DtoLayer.ProductDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        private readonly IMapper _mapper;
+
+        public ProductController(IProductService productService, IMapper mapper)
+        {
+            _productService = productService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult ProductList()
+        {
+            var values = _mapper.Map<List<ResultProductDto>>(_productService.TGetListAll());
+            return Ok(values);
+        }
+
+        [HttpGet("ProductListWithCategory")]
+        public IActionResult ProductListWithCategory()
+        {
+            var context = new SignalRContext();
+            var values = context.Products.Include(x => x.Category).Select(y => new ResultProductWithCategory()
+            {
+                Description = y.Description,
+                ImageUrl = y.ImageUrl,
+                Price = y.Price,
+                ProductID = y.ProductID,
+                ProductName = y.ProductName,
+                ProductStatus = y.ProductStatus,
+                CategoryName = y.Category.CategoryName
+            });
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateProduct(CreateProductDto createProductDto)
+        {
+            _productService.TAdd(new Product()
+            {
+                ProductName = createProductDto.ProductName,
+                Description = createProductDto.Description,
+                Price = createProductDto.Price,
+                ImageUrl = createProductDto.ImageUrl,
+                ProductStatus = true
+            });
+            return Ok("Ürün Bilgisi Eklendi.");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteProduct(int id)
+        {
+            var value = _productService.TGetByID(id);
+            _productService.TDelete(value);
+            return Ok("Ürün Bilgisi Silindi");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateProduct(UpdateProductDto updateProductDto)
+        {
+            _productService.TUpdate(new Product()
+            {
+                ProductID = updateProductDto.ProductID,
+                ProductName = updateProductDto.ProductName,
+                Description = updateProductDto.Description,
+                Price = updateProductDto.Price,
+                ImageUrl = updateProductDto.ImageUrl,
+                ProductStatus = updateProductDto.ProductStatus,
+            });
+            return Ok("Ürün Bilgisi Güncellendi");
+        }
+
+        [HttpGet("GetProduct")]
+        public IActionResult GetProduct(int id) 
+        {
+            var value = _productService.TGetByID(id);
+            return Ok(value);
+        }
+    }
+}
