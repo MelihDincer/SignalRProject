bbce91f2b17c67a8a41cfd41ff61125d83189ef5
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-02 10:21:33 +0300

    TestimonialController'ın oluşturulması ve API kısmının tamamlanması.

---
+++ SignalRApi/Controllers/TestimonialController.cs
@@ -0,0 +1,73 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.TestimonialDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestimonialController : ControllerBase
+    {
+        private readonly ITestimonialService _testimonialService;
+        private readonly IMapper _mapper;
+
+        public TestimonialController(ITestimonialService testimonialService, IMapper mapper)
+        {
+            _testimonialService = testimonialService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult TestimonialList()
+        {
+            var values = _mapper.Map<List<ResultTestimonialDto>>(_testimonialService.TGetListAll());
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateTestimonial(CreateTestimonialDto createTestimonialDto)
+        {
+            _testimonialService.TAdd(new Testimonial()
+            {
+                Name = createTestimonialDto.Name,
+                Title = createTestimonialDto.Title,
+                Comment = createTestimonialDto.Comment,
+                ImageUrl = createTestimonialDto.ImageUrl,
+                Status = true
+            });
+            return Ok("Müşteri Yorum Bilgisi Eklendi");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteTestimonial(int id)
+        {
+            var value = _testimonialService.TGetByID(id);
+            _testimonialService.TDelete(value);
+            return Ok("Müşteri Yorum Bilgisi Silindi");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateTestimonial(UpdateTestimonialDto updateTestimonialDto)
+        {
+            _testimonialService.TUpdate(new Testimonial()
+            {
+                TestimonialID = updateTestimonialDto.TestimonialID,
+                Title = updateTestimonialDto.Title,
+                Comment = updateTestimonialDto.Comment,
+                ImageUrl = updateTestimonialDto.ImageUrl,
+                Status = updateTestimonialDto.Status
+            });
+            return Ok("Müşteri Yorum Bilgisi Güncellendi");
+        }
+
+        [HttpGet("GetTestimonial")]
+        public IActionResult GetTestimonial(int id)
+        {
+            var value = _testimonialService.TGetByID(id);
+            return Ok(value);
+        }
+    }
+}
