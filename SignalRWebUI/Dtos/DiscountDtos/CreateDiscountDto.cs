ede615603bb1f13902226ee4d2b6c37224674ed9
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-03 13:58:15 +0300

    WebUI tarafında Discount Dtolarının eklenmesi.

---
+++ SignalRWebUI/Dtos/DiscountDtos/CreateDiscountDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SignalRWebUI.Dtos.DiscountDtos
+{
+    public class CreateDiscountDto
+    {
+        public string Title { get; set; }
+        public string Amount { get; set; } //İndirim oranı
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ SignalRWebUI/Dtos/DiscountDtos/UpdateDiscountDto.cs
@@ -0,0 +1,11 @@
+﻿namespace SignalRWebUI.Dtos.DiscountDtos
+{
+    public class UpdateDiscountDto
+    {
+        public int DiscountID { get; set; }
+        public string Title { get; set; }
+        public string Amount { get; set; } //İndirim oranı
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
