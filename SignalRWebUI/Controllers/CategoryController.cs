7eac54b2842bc380306288de39e73fdbe0ae13f0
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-03 13:56:43 +0300

    WebUI tarafında CategoryController'ın eklenmesi.

---
+++ SignalRWebUI/Controllers/CategoryController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.CategoryDtos;
+using System.Text;
+
+namespace SignalRWebUI.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public CategoryController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient(); //Client(istemci) oluşturuldu.
+            var responseMessage = await client.GetAsync("https://localhost:7029/api/Category"); //Listeleme işlemi için GetAsync isteğinde bulunduk.
+            if (responseMessage.IsSuccessStatusCode) //Başarılı durum kodu dönerse
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync(); //Response'dan gelen veriyi jsonData değişkenine atadık.
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData); //Json türündeki bu veriyi deserialize ederek tabloda görüntüleyebileceğimiz bir türe dönüştürdük.
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryDto createCategoryDto)
+        {
+            createCategoryDto.CategoryStatus = true;
+            var client = _httpClientFactory.CreateClient(); //Client oluşturduk.
+            var jsonData = JsonConvert.SerializeObject(createCategoryDto); //createCategoryDto dan gelen nesne örneğini json türe dönüştürdük.
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json"); //İçeriğimizin dönüşümü için StringContent sınıfından bir nesne türetip üçüncü overloadını bu şekilde kullandık. (içerik, türkçe karakteri destekle, medya türü)
+            var responseMessage = await client.PostAsync("https://localhost:7029/api/Category", stringContent); //Ekleme işlemi için PostAsync isteğinde bulunduk.
+            if (responseMessage.IsSuccessStatusCode) //Başarılı durum kodu dönerse
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> DeleteCategory(int id)
+        {
+            var client = _httpClientFactory.CreateClient(); //Client oluşturduk
+            var responseMessage = await client.DeleteAsync($"https://localhost:7029/api/Category?id={id}"); //Silme işlemi için DeleteAsync isteğinde bulunduk.
+            if (responseMessage.IsSuccessStatusCode) //Başarılı durum kodu dönerse
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateCategory(int id)
+        {
+            var client = _httpClientFactory.CreateClient(); //Client oluşturduk.
+            var responseMessage = await client.GetAsync($"https://localhost:7029/api/Category/GetCategory?id={id}"); //Güncelleme sayfasında kategori bilgisini getirmek için GetAsync isteğinde bulunduk.
+            if (responseMessage.IsSuccessStatusCode) //Başarılı durum kodu dönerse
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();//Jsondan gelen içeriği string formatında okuma
+                var values = JsonConvert.DeserializeObject<UpdateCategoryDto>(jsonData); //Json türündeki bu veriyi deserialize ederek tabloda görüntüleyebileceğimiz bir türe dönüştürdük.
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateCategory(UpdateCategoryDto updateCategoryDto)
+        {
+            var client = _httpClientFactory.CreateClient(); //Client oluşturduk.
+            var jsonData = JsonConvert.SerializeObject(updateCategoryDto); //updateCategoryDto dan gelen değerleri json türüne dönüştürdük yani serilize ettik.
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json"); //İçeriğimizin dönüşümü için StringContent sınıfından bir nesne türetip üçüncü overloadını bu şekilde kullandık. (içerik, türkçe karakteri destekle, medya türü)
+            var responseMessage = await client.PutAsync("https://localhost:7029/api/Category", stringContent); //Güncelleme işlemi için PutAsync isteğinde bulunduk.
+            if (responseMessage.IsSuccessStatusCode) //Başarılı durum kodu dönerse
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
