1a91436cea2ad55ef5dddb1b6e3050dd25fc2aca
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-03 13:56:56 +0300

    WebUI tarafında DiscountController'ın eklenmesi.

---
+++ SignalRWebUI/Controllers/DiscountController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.DiscountDtos;
+using System.Text;
+
+namespace SignalRWebUI.Controllers
+{
+    public class DiscountController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public DiscountController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient(); //Client(istemci) oluşturuldu.
+            var responseMessage = await client.GetAsync("https://localhost:7029/api/Discount"); //Listeleme işlemi için GetAsync isteğinde bulunduk.
+            if (responseMessage.IsSuccessStatusCode) //Başarılı durum kodu dönerse
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync(); //Response'dan gelen veriyi jsonData değişkenine atadık.
+                var values = JsonConvert.DeserializeObject<List<ResultDiscountDto>>(jsonData); //Json türündeki bu veriyi deserialize ederek tabloda görüntüleyebileceğimiz bir türe dönüştürdük.
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateDiscount()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateDiscount(CreateDiscountDto createDiscountDto)
+        {
+            var client = _httpClientFactory.CreateClient(); //Client oluşturduk.
+            var jsonData = JsonConvert.SerializeObject(createDiscountDto); //createDiscountDto dan gelen nesne örneğini json türe dönüştürdük.
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json"); //İçeriğimizin dönüşümü için StringContent sınıfından bir nesne türetip üçüncü overloadını bu şekilde kullandık. (içerik, türkçe karakteri destekle, medya türü)
+            var responseMessage = await client.PostAsync("https://localhost:7029/api/Discount", stringContent); //Ekleme işlemi için PostAsync isteğinde bulunduk.
+            if (responseMessage.IsSuccessStatusCode) //Başarılı durum kodu dönerse
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> DeleteDiscount(int id)
+        {
+            var client = _httpClientFactory.CreateClient(); //Client oluşturduk
+            var responseMessage = await client.DeleteAsync($"https://localhost:7029/api/Discount?id={id}"); //Silme işlemi için DeleteAsync isteğinde bulunduk.
+            if (responseMessage.IsSuccessStatusCode) //Başarılı durum kodu dönerse
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateDiscount(int id)
+        {
+            var client = _httpClientFactory.CreateClient(); //Client oluşturduk.
+            var responseMessage = await client.GetAsync($"https://localhost:7029/api/Discount/GetDiscount?id={id}"); //Güncelleme sayfasında kategori bilgisini getirmek için GetAsync isteğinde bulunduk.
+            if (responseMessage.IsSuccessStatusCode) //Başarılı durum kodu dönerse
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();//Jsondan gelen içeriği string formatında okuma
+                var values = JsonConvert.DeserializeObject<UpdateDiscountDto>(jsonData); //Json türündeki bu veriyi deserialize ederek tabloda görüntüleyebileceğimiz bir türe dönüştürdük.
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateDiscount(UpdateDiscountDto updateDiscountDto)
+        {
+            var client = _httpClientFactory.CreateClient(); //Client oluşturduk.
+            var jsonData = JsonConvert.SerializeObject(updateDiscountDto); //updateDiscountDto dan gelen değerleri json türüne dönüştürdük yani serilize ettik.
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json"); //İçeriğimizin dönüşümü için StringContent sınıfından bir nesne türetip üçüncü overloadını bu şekilde kullandık. (içerik, türkçe karakteri destekle, medya türü)
+            var responseMessage = await client.PutAsync("https://localhost:7029/api/Discount", stringContent); //Güncelleme işlemi için PutAsync isteğinde bulunduk.
+            if (responseMessage.IsSuccessStatusCode) //Başarılı durum kodu dönerse
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
