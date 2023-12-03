b06d325d65c3a2320cfcdac2f756c8059e506d8a
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-03 13:57:49 +0300

    WebUI tarafında About Dtolarının eklenmesi.

---
+++ SignalRWebUI/Dtos/AboutDtos/UpdateAboutDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SignalRWebUI.Dtos.AboutDtos
+{
+    public class UpdateAboutDto
+    {
+        public int AboutID { get; set; }
+        public string ImageUrl { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
