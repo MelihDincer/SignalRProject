cd0b4c06358e5228a83f625818801c8bbcc840d1
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-04 23:51:47 +0300

    EfOrderDal oluşturulması.

---
+++ SignalR.DataAccessLayer/EntityFramework/EfOrderDal.cs
@@ -0,0 +1,38 @@
+﻿using SignalR.DataAccessLayer.Abstract;
+using SignalR.DataAccessLayer.Concrete;
+using SignalR.DataAccessLayer.Repositories;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalR.DataAccessLayer.EntityFramework
+{
+    public class EfOrderDal : GenericRepository<Order>, IOrderDal
+    {
+        public EfOrderDal(SignalRContext context) : base(context)
+        {
+        }
+
+        public int ActiveOrderCount()
+        {
+            using var context = new SignalRContext();
+            return context.Orders.Where(x => x.Description == "Müşteri Masada").Count();
+        }
+
+        public decimal LastOrderTotalPrice()
+        {
+            using var context = new SignalRContext();
+            return context.Orders.Take(1).OrderByDescending(x => x.OrderID).Select(y => y.TotalPrice).FirstOrDefault();
+        }
+
+        public decimal TodayTotalPrice()
+        {
+            using var context = new SignalRContext();
+            return context.Orders.Where(x => x.Description == "Hesap Kapatıldı" && x.Date.Day == DateTime.Now.Day && x.Date.Month == DateTime.Now.Month && x.Date.Year == DateTime.Now.Year).Sum(y => y.TotalPrice);
+        }
+
+        public int TotalOrderCount()
+        {
+            using var context = new SignalRContext();
+            return context.Orders.Count();
+        }
+    }
+}
