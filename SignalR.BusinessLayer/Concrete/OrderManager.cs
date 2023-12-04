9a14d8be0c1c24ae3995fdf4101aaf36a3c740bb
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-04 23:47:07 +0300

    OrderManager oluşturulması.

---
+++ SignalR.BusinessLayer/Concrete/OrderManager.cs
@@ -0,0 +1,61 @@
+﻿using SignalR.BusinessLayer.Abstract;
+using SignalR.DataAccessLayer.Abstract;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalR.BusinessLayer.Concrete
+{
+    public class OrderManager : IOrderService
+    {
+        private readonly IOrderDal _orderDal;
+
+        public OrderManager(IOrderDal orderDal)
+        {
+            _orderDal = orderDal;
+        }
+
+        public int TActiveOrderCount()
+        {
+            return _orderDal.ActiveOrderCount();
+        }
+
+        public void TAdd(Order entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Order entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Order TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Order> TGetListAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal TLastOrderTotalPrice()
+        {
+            return _orderDal.LastOrderTotalPrice();
+        }
+
+        public decimal TTodayTotalPrice()
+        {
+            return _orderDal.TodayTotalPrice();
+        }
+
+        public int TTotalOrderCount()
+        {
+            return _orderDal.TotalOrderCount();
+        }
+
+        public void TUpdate(Order entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
