75aa58e42e2c03cc2f54575cfc9974e70f913ad8
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-04 20:49:53 +0300

    MenuTable entitysinin oluşturulması.

---
+++ SignalR.EntityLayer/Entities/MenuTable.cs
@@ -0,0 +1,9 @@
+﻿namespace SignalR.EntityLayer.Entities
+{
+    public class MenuTable
+    {
+        public int MenuTableID { get; set; }
+        public string Name{ get; set; }
+        public bool Status { get; set; }
+    }
+}
