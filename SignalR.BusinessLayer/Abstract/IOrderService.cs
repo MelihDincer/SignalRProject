9cadb594dbb0257bf4ddc528833c599a6b7b7312
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-04 23:46:27 +0300

    IOrderService'ın oluşturulması.

---
+++ SignalR.BusinessLayer/Abstract/IOrderService.cs
@@ -0,0 +1,12 @@
+﻿using SignalR.EntityLayer.Entities;
+
+namespace SignalR.BusinessLayer.Abstract
+{
+    public interface IOrderService : IGenericService<Order>
+    {
+        int TTotalOrderCount();
+        int TActiveOrderCount();
+        decimal TLastOrderTotalPrice();
+        decimal TTodayTotalPrice();
+    }
+}
