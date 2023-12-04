3683966bd210e63e0a02ebe6648a263b150e41a1
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-04 20:41:42 +0300

    IProductService'da entitye özgü metot imzalarının tanımlanması.

--- SignalR.BusinessLayer/Abstract/IProductService.cs
+++ SignalR.BusinessLayer/Abstract/IProductService.cs
@@ -5,5 +5,12 @@ namespace SignalR.BusinessLayer.Abstract
     public interface IProductService : IGenericService<Product>
     {
         List<Product> TGetProductsWithCategories();
+        int TProductCount();
+        int TProductCountByCategoryNameHamburger();
+        int TProductCountByCategoryNameDrink();
+        decimal TProductPriceAvg();
+        string TProductNameByMaxPrice();
+        string TProductNameByMinPrice();
+        decimal TProductPriceAvgByCategoryNameHamburger();
     }
 }
