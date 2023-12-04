cb67b0db6e10eed2cc8faf579669f94faf945c50
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-04 20:41:27 +0300

    ICategoryService'da entitye özgü metot imzalarının tanımlanması.

--- SignalR.BusinessLayer/Abstract/ICategoryService.cs
+++ SignalR.BusinessLayer/Abstract/ICategoryService.cs
@@ -4,5 +4,8 @@ namespace SignalR.BusinessLayer.Abstract
 {
     public interface ICategoryService : IGenericService<Category>
     {
+        int TCategoryCount();
+        int TActiveCategoryCount();
+        int TPassiveCategoryCount();
     }
 }
