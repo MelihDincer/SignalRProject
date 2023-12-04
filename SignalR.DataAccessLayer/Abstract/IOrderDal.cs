c7719a9dbf881db7841fff8260fa52f46eacbdc9
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-04 23:51:02 +0300

    IOrderDal oluşturulması.

---
+++ SignalR.DataAccessLayer/Abstract/IOrderDal.cs
@@ -0,0 +1,12 @@
+﻿using SignalR.EntityLayer.Entities;
+
+namespace SignalR.DataAccessLayer.Abstract
+{
+    public interface IOrderDal : IGenericDal<Order>
+    {
+        int TotalOrderCount();
+        int ActiveOrderCount();
+        decimal LastOrderTotalPrice();
+        decimal TodayTotalPrice();
+    }
+}
