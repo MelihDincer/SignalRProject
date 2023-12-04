2ac54f2049c49128ccd8bf11801df9d03062d640
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-04 23:48:29 +0300

    IMenuTableDal oluşturulması.

---
+++ SignalR.DataAccessLayer/Abstract/IMenuTableDal.cs
@@ -0,0 +1,9 @@
+﻿using SignalR.EntityLayer.Entities;
+
+namespace SignalR.DataAccessLayer.Abstract
+{
+    public interface IMenuTableDal : IGenericDal<MenuTable>
+    {
+        int MenuTableCount();
+    }
+}
