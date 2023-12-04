6683c152526c26cad4e7471c64ea2e53b3cdfdcd
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-04 20:44:35 +0300

    IProductDal interfaceinde tanımlanan metot imzalarının EfProductDal sınıfında implemente edilmesi.

--- SignalR.DataAccessLayer/EntityFramework/EfProductDal.cs
+++ SignalR.DataAccessLayer/EntityFramework/EfProductDal.cs
@@ -21,5 +21,49 @@ namespace SignalR.DataAccessLayer.EntityFramework
             var values = context.Products.Include(x => x.Category).ToList(); //Category tablosunu Products'a dahil et ve listele.
             return values;
         }
+
+        public int ProductCount()
+        {
+            using var context = new SignalRContext();
+            return context.Products.Count();
+        }
+
+        public int ProductCountByCategoryNameDrink()
+        {
+            using var context = new SignalRContext();
+            return context.Products.Count(x => x.CategoryID == (context.Categories.Where(y => y.CategoryName == "İçecek").Select(z => z.CategoryID)).FirstOrDefault());
+
+        }
+
+        public int ProductCountByCategoryNameHamburger()
+        {
+            using var context = new SignalRContext();
+            return context.Products.Count(x => x.CategoryID == (context.Categories.Where(y => y.CategoryName == "Hamburger").Select(z => z.CategoryID)).FirstOrDefault());
+        }
+
+        public string ProductNameByMaxPrice()
+        {
+            using var context = new SignalRContext();
+            return context.Products.Where(x => x.Price == (context.Products.Max(y => y.Price))).Select(z => z.ProductName).FirstOrDefault();
+        }
+
+        public string ProductNameByMinPrice()
+        {
+            using var context = new SignalRContext();
+            return context.Products.Where(x => x.Price == (context.Products.Min(y => y.Price))).Select(z => z.ProductName).FirstOrDefault();
+
+        }
+
+        public decimal ProductPriceAvg()
+        {
+            using var context = new SignalRContext();
+            return context.Products.Average(x => x.Price);
+        }
+
+        public decimal ProductPriceAvgByCategoryNameHamburger()
+        {
+            using var context = new SignalRContext();
+            return context.Products.Where(x => x.CategoryID == (context.Categories.Where(y => y.CategoryName == "Hamburger").Select(z => z.CategoryID).FirstOrDefault())).Average(g => g.Price);
+        }
     }
 }
