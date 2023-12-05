daca3b9c9ea399470f9f81ae82a3a2d44e5fe4b1
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-05 09:16:22 +0300

    Product Api Controller'da istatistik verilerin test edilmesi.

--- SignalRApi/Controllers/ProductController.cs
+++ SignalRApi/Controllers/ProductController.cs
@@ -29,6 +29,48 @@ namespace SignalRApi.Controllers
             return Ok(values);
         }
 
+        [HttpGet("ProductCount")]
+        public IActionResult ProductCount()
+        {
+            return Ok(_productService.TProductCount());
+        }
+
+        [HttpGet("ProductCountByHambuger")]
+        public IActionResult ProductCountByHambuger()
+        {
+            return Ok(_productService.TProductCountByCategoryNameHamburger());
+        }
+
+        [HttpGet("ProductCountByDrink")]
+        public IActionResult ProductCountByDrink()
+        {
+            return Ok(_productService.TProductCountByCategoryNameDrink());
+        }
+
+        [HttpGet("ProductPriceAvg")]
+        public IActionResult ProductPriceAvg()
+        {
+            return Ok(_productService.TProductPriceAvg());
+        }
+
+        [HttpGet("ProductNameByMinPrice")]
+        public IActionResult ProductNameByMinPrice()
+        {
+            return Ok(_productService.TProductNameByMinPrice());
+        }
+
+        [HttpGet("ProductNameByMaxPrice")]
+        public IActionResult ProductNameByMaxPrice()
+        {
+            return Ok(_productService.TProductNameByMaxPrice());
+        }
+
+        [HttpGet("ProductPriceAvgByCategoryNameHamburger")]
+        public IActionResult ProductPriceAvgByCategoryNameHamburger()
+        {
+            return Ok(_productService.TProductPriceAvgByCategoryNameHamburger());
+        }
+
         [HttpGet("ProductListWithCategory")]
         public IActionResult ProductListWithCategory()
         {
