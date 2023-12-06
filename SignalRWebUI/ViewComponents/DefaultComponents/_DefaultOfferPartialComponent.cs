8d098062599b31e3b4e38a1b8b0868540e161e2d
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-06 16:22:26 +0300

    Default sayfası ViewComponentlere bölündü. İndirim bölümü tablo verilerinin çekilmesi için consume işlemi yapıldı.

---
+++ SignalRWebUI/ViewComponents/DefaultComponents/_DefaultOfferPartialComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.DiscountDtos;
+
+namespace SignalRWebUI.ViewComponents.DefaultComponents
+{
+    public class _DefaultOfferPartialComponent : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultOfferPartialComponent(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7029/api/Discount");
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultDiscountDto>>(jsonData);
+            return View(values);
+        }
+    }
+}
