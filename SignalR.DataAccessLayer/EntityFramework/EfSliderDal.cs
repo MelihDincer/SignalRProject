83dd2f03518a26698910feb4fd9f23b5b3b142c0
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-06 16:08:34 +0300

    EfSliderDal oluşturuldu.

---
+++ SignalR.DataAccessLayer/EntityFramework/EfSliderDal.cs
@@ -0,0 +1,14 @@
+﻿using SignalR.DataAccessLayer.Abstract;
+using SignalR.DataAccessLayer.Concrete;
+using SignalR.DataAccessLayer.Repositories;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalR.DataAccessLayer.EntityFramework
+{
+    public class EfSliderDal : GenericRepository<Slider>, ISliderDal
+    {
+        public EfSliderDal(SignalRContext context) : base(context)
+        {
+        }
+    }
+}
