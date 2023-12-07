781f216ed356cfd4aa478417a8e0ac7a08138b94
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-07 22:03:14 +0300

    EfBasketDal oluşturuldu. Include işlemi yapıldı.

---
+++ SignalR.DataAccessLayer/EntityFramework/EfBasketDal.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SignalR.DataAccessLayer.Abstract;
+using SignalR.DataAccessLayer.Concrete;
+using SignalR.DataAccessLayer.Repositories;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalR.DataAccessLayer.EntityFramework
+{
+    public class EfBasketDal : GenericRepository<Basket>, IBasketDal
+    {
+        public EfBasketDal(SignalRContext context) : base(context)
+        {
+        }
+
+        public List<Basket> GetBasketByMenuTableNumber(int id)
+        {
+            using var context = new SignalRContext();
+            var values = context.Baskets.Where(x => x.MenuTableID == id).Include(y=>y.Product).ToList();
+            return values;
+        }
+    }
+}
