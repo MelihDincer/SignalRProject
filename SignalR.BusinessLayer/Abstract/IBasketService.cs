da294a24f1adf425ef1ad3d68783a4b413e7664c
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-07 22:01:14 +0300

    IBaskerService oluşturuldu.

---
+++ SignalR.BusinessLayer/Abstract/IBasketService.cs
@@ -0,0 +1,9 @@
+﻿using SignalR.EntityLayer.Entities;
+
+namespace SignalR.BusinessLayer.Abstract
+{
+    public interface IBasketService : IGenericService<Basket>
+    {
+        List<Basket> TGetBasketByMenuTableNumber(int id);
+    }
+}
