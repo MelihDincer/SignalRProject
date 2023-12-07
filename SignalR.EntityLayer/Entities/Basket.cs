6ca0752d60392f9060f406957ffbd3ebbe84bdc2
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-07 21:59:37 +0300

    Basket tablosu eklendi.

---
+++ SignalR.EntityLayer/Entities/Basket.cs
@@ -0,0 +1,14 @@
+﻿namespace SignalR.EntityLayer.Entities
+{
+    public class Basket
+    {
+        public int BasketID { get; set; }
+        public decimal Price { get; set; }
+        public decimal Count { get; set; }
+        public decimal TotalPrice { get; set; }
+        public int ProductID { get; set; }
+        public Product Product { get; set; }
+        public int MenuTableID{ get; set; }
+        public MenuTable MenuTable { get; set; }
+    }
+}
