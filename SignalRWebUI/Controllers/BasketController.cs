32a13850e4652cd29c5b07e5ac1646ee25d26a70
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-07 22:23:32 +0300

    UI tarafında BasketController'ın oluşturulması ve consume işleminin yapılması.

---
+++ SignalRWebUI/Controllers/BasketController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.BasketDtos;
+
+namespace SignalRWebUI.Controllers
+{
+    public class BasketController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public BasketController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7029/api/Baskets/BasketListByMenuTableWithProductName?id=4");
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBasketDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        
+    }
+}
