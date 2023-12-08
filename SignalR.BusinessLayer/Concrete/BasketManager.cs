9614bcd6f5f514ebd96743c510a95a6a79d82391
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-08 14:04:46 +0300

    BasketManager'da TDelete ve TGetByID metotlarının yazılması.

--- SignalR.BusinessLayer/Concrete/BasketManager.cs
+++ SignalR.BusinessLayer/Concrete/BasketManager.cs
@@ -25,7 +25,7 @@ namespace SignalR.BusinessLayer.Concrete
 
         public void TDelete(Basket entity)
         {
-            throw new NotImplementedException();
+            _basketDal.Delete(entity);
         }
 
         public List<Basket> TGetBasketByMenuTableNumber(int id)
@@ -35,7 +35,7 @@ namespace SignalR.BusinessLayer.Concrete
 
         public Basket TGetByID(int id)
         {
-            throw new NotImplementedException();
+            return _basketDal.GetByID(id);
         }
 
         public List<Basket> TGetListAll()
