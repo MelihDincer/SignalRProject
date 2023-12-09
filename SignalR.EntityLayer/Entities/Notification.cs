b0365a80f27f60fcad949adbfc405266689e4adf
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-09 15:46:30 +0300

    Notification entity sınıfının eklenmesi.

---
+++ SignalR.EntityLayer/Entities/Notification.cs
@@ -0,0 +1,12 @@
+﻿namespace SignalR.EntityLayer.Entities
+{
+    public class Notification
+    {
+        public int NotificationID { get; set; }
+        public string Type { get; set; }
+        public string Icon { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public bool Status { get; set; }
+    }
+}
