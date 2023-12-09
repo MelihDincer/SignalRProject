18500e432b15805bddcaded52906d345da28fb75
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-09 15:45:05 +0300

    Notifications tablosunun eklenmesi.

--- SignalR.DataAccessLayer/Concrete/SignalRContext.cs
+++ SignalR.DataAccessLayer/Concrete/SignalRContext.cs
@@ -25,5 +25,6 @@ namespace SignalR.DataAccessLayer.Concrete
         public DbSet<MenuTable> MenuTables { get; set; }
         public DbSet<Slider> Sliders { get; set; }
         public DbSet<Basket> Baskets { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
     }
 }
