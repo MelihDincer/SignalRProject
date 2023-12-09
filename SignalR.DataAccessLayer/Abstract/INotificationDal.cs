f7c1e6dbb63c96a0cb04d357f2a007ab0b1f6da4
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-09 15:44:41 +0300

    INotificationDal oluşturuldu.

---
+++ SignalR.DataAccessLayer/Abstract/INotificationDal.cs
@@ -0,0 +1,15 @@
+﻿using SignalR.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.DataAccessLayer.Abstract
+{
+    public interface INotificationDal : IGenericDal<Notification>
+    {
+        int NotificationCountByStatusFalse(); //Durumu okunmadı olan bildirimlerin sayısı
+        List<Notification> GetAllNotificationByFalse(); //Durumu okunmadı olan bildirimlerin listesi
+    }
+}
