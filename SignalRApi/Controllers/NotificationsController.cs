9f8f45f5745567c33f18201ec95010b82753c9b8
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-09 15:46:43 +0300

    Notifications API Controllerının eklenmesi.

---
+++ SignalRApi/Controllers/NotificationsController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.NotificationDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationsController : ControllerBase
+    {
+        private readonly INotificationService _notificationService;
+
+        public NotificationsController(INotificationService notificationService)
+        {
+            _notificationService = notificationService;
+        }
+
+        [HttpGet]
+        public IActionResult NotificationList() 
+        { 
+            return Ok(_notificationService.TGetListAll());
+        }
+
+        [HttpGet("NotificationCountByStatusFalse")]
+        public IActionResult NotificationCountByStatusFalse()
+        {
+            return Ok(_notificationService.TNotificationCountByStatusFalse());
+        }
+
+        [HttpGet("GetAllNotificationByFalse")]
+        public IActionResult GetAllNotificationByFalse()
+        {
+            return Ok(_notificationService.TGetAllNotificationByFalse());
+        }
+
+        [HttpPost]
+        public IActionResult CreateNotification(CreateNotificationDto createNotificationDto)
+        {
+            _notificationService.TAdd(new Notification
+            {
+                Type = createNotificationDto.Type,
+                Icon = createNotificationDto.Icon,
+                Description = createNotificationDto.Description,
+                Date = Convert.ToDateTime(DateTime.Now.ToShortDateString()),
+                Status = false
+            });
+            return Ok("Yeni Bildirim Eklendi");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteNotification(int id)
+        {
+            var value = _notificationService.TGetByID(id);
+            _notificationService.TDelete(value);
+            return Ok("Bildirim Silindi");
+        }
+
+        [HttpPut]
+        public IActionResult  UpdateNotification(UpdateNotificationDto updateNotificationDto)
+        {
+            _notificationService.TUpdate(new Notification
+            {
+                NotificationID = updateNotificationDto.NotificationID,
+                Type = updateNotificationDto.Type,
+                Icon = updateNotificationDto.Icon,
+                Description = updateNotificationDto.Description,
+                Date = updateNotificationDto.Date,
+                Status = updateNotificationDto.Status
+            });
+            return Ok("Güncelleme Başarılı");
+        }
+
+        [HttpGet("GetNotification")]
+        public IActionResult GetNotification(int id)
+        {
+            var value = _notificationService.TGetByID(id);
+            return Ok(value);
+        }
+    }
+}
