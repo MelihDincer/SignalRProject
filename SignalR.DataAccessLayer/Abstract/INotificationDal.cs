6f67c01985cac941070ef65e9acb8647db649ace
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-10 22:58:39 +0300

    INotificationDal interfaceinde bildirimin durumunu güncelleyecek iki metot imzası tanımlandı.

--- SignalR.DataAccessLayer/Abstract/INotificationDal.cs
+++ SignalR.DataAccessLayer/Abstract/INotificationDal.cs
@@ -11,5 +11,7 @@ namespace SignalR.DataAccessLayer.Abstract
     {
         int NotificationCountByStatusFalse(); //Durumu okunmadı olan bildirimlerin sayısı
         List<Notification> GetAllNotificationByFalse(); //Durumu okunmadı olan bildirimlerin listesi
+        void NotificationChangeToTrue(int id); //Bildirimin türünü true yap. (Okundu)
+        void NotificationChangeToFalse(int id); //Bildirimin türünü false yap. (Okunmadı)
     }
 }
