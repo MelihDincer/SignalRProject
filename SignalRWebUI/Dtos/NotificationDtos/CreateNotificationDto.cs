868a6252804a76dd93f1368870352fdf59427f70
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-10 23:03:15 +0300

    NotificationDto sınıflarının eklenmesi.

---
+++ SignalRWebUI/Dtos/NotificationDtos/CreateNotificationDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SignalRWebUI.Dtos.NotificationDtos
+{
+    public class CreateNotificationDto
+    {
+        public string Type { get; set; }
+        public string Icon { get; set; }
+        public string Description { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ SignalRWebUI/Dtos/NotificationDtos/UpdateNotificationDto.cs
@@ -0,0 +1,12 @@
+﻿namespace SignalRWebUI.Dtos.NotificationDtos
+{
+    public class UpdateNotificationDto
+    {
+        public int NotificationID { get; set; }
+        public string Type { get; set; }
+        public string Icon { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public bool Status { get; set; }
+    }
+}
