ed5d67c0df9c5685d961eb5c766f8a4ae9b28d11
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-10 23:01:10 +0300

    Count değeri int olarak revize edildi.

--- SignalR.EntityLayer/Entities/Basket.cs
+++ SignalR.EntityLayer/Entities/Basket.cs
@@ -4,7 +4,7 @@
     {
         public int BasketID { get; set; }
         public decimal Price { get; set; }
-        public decimal Count { get; set; }
+        public int Count { get; set; }
         public decimal TotalPrice { get; set; }
         public int ProductID { get; set; }
         public Product Product { get; set; }
