fa86275a5d618a299e51a9df2898bc12f1536bbc
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-10 23:29:57 +0300

    MenuTableManager sınıfında crud işlemlerinin yapılması.

--- SignalR.BusinessLayer/Concrete/MenuTableManager.cs
+++ SignalR.BusinessLayer/Concrete/MenuTableManager.cs
@@ -20,22 +20,22 @@ namespace SignalR.BusinessLayer.Concrete
 
         public void TAdd(MenuTable entity)
         {
-            throw new NotImplementedException();
+            _menuTableDal.Add(entity);
         }
 
         public void TDelete(MenuTable entity)
         {
-            throw new NotImplementedException();
+            _menuTableDal.Delete(entity);
         }
 
         public MenuTable TGetByID(int id)
         {
-            throw new NotImplementedException();
+            return _menuTableDal.GetByID(id);
         }
 
         public List<MenuTable> TGetListAll()
         {
-            throw new NotImplementedException();
+            return _menuTableDal.GetListAll();
         }
 
         public int TMenuTableCount()
@@ -45,7 +45,7 @@ namespace SignalR.BusinessLayer.Concrete
 
         public void TUpdate(MenuTable entity)
         {
-            throw new NotImplementedException();
+            _menuTableDal.Update(entity);
         }
     }
 }
