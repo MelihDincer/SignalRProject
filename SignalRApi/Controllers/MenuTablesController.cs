62339156a7977f41d2f2ed8294be95d6c870f32a
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-11 17:14:05 +0300

    Masa bilgilerinin API tarafında crud işlemlerinin gerçekleştirilmesi.

--- SignalRApi/Controllers/MenuTablesController.cs
+++ SignalRApi/Controllers/MenuTablesController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.MenuTableDto;
+using SignalR.EntityLayer.Entities;
 
 namespace SignalRApi.Controllers
 {
@@ -19,6 +21,50 @@ namespace SignalRApi.Controllers
         public IActionResult MenuTableCount()
         {
             return Ok(_menuTableService.TMenuTableCount());
+        }
+
+        [HttpGet]
+        public IActionResult MenuTableList()
+        {
+            var values = _menuTableService.TGetListAll();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateMenuTable(CreateMenuTableDto createMenuTableDto)
+        {
+            _menuTableService.TAdd(new MenuTable
+            {
+                Name = createMenuTableDto.Name,
+                Status = false //Müşteri yok anlamında
+            });
+            return Ok("Masa başarılı bir şekilde eklendi");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteMenuTable(int id)
+        {
+            var value = _menuTableService.TGetByID(id);
+            _menuTableService.TDelete(value);
+            return Ok("Masa başarılı bir şekilde silindi");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateMenuTable(UpdateMenuTableDto updateMenuTableDto)
+        {
+            _menuTableService.TUpdate(new MenuTable
+            {
+                MenuTableID = updateMenuTableDto.MenuTableID,
+                Name = updateMenuTableDto.Name,
+                Status = false
+            });
+            return Ok("Masa başarılı bir şekilde güncellendi");
+        }
+
+        [HttpGet("GetMenuTable")]
+        public IActionResult GetMenuTable(int id)
+        {
+            return Ok(_menuTableService.TGetByID(id));
         }
     }
 }
