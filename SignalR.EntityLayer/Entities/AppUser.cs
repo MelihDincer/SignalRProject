8b427555c9702173056dd11aceb2574833dbf1da
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-12 21:52:56 +0300

    Identity için AppUser entitysinin oluşturulması.

---
+++ SignalR.EntityLayer/Entities/AppUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SignalR.EntityLayer.Entities
+{
+    public class AppUser : IdentityUser<int> //Default olarak ID'yi string olarak verir. Biz int olarak verdik.
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
