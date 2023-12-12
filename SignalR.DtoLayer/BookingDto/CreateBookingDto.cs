72f36bc43c237044b34c7f70fff10f42959e266e
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-12 21:52:33 +0300

    Rezervasyon işleminde statik bir biçimde yazılmış rezervasyon açıklaması, dinamik yapılabilmesi için property olarak eklenmesi.

--- SignalR.DtoLayer/BookingDto/CreateBookingDto.cs
+++ SignalR.DtoLayer/BookingDto/CreateBookingDto.cs
@@ -3,6 +3,7 @@
     public class CreateBookingDto
     {
         public string Name { get; set; }
+        public string Description { get; set; }
         public string Phone { get; set; }
         public string Mail { get; set; }
         public int PersonCount { get; set; }
