daa7d7a68b1c9f4f8aaeb3f66f30c62189672495
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-13 23:09:01 +0300

    MessageController API eklendi.

---
+++ SignalRApi/Controllers/MessageController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.MessageDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        private readonly IMessageService _messageService;
+
+        public MessageController(IMessageService messageService)
+        {
+            _messageService = messageService;
+        }
+
+        [HttpGet]
+        public IActionResult MessageList()
+        {
+            var values = _messageService.TGetListAll();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateMessage(CreateMessageDto createMessageDto)
+        {
+            _messageService.TAdd(new Message
+            {
+                Mail = createMessageDto.Mail,
+                MessageContent = createMessageDto.MessageContent,
+                MessageSendDate = DateTime.Now,
+                NameSurname = createMessageDto.NameSurname,
+                Phone = createMessageDto.Phone,
+                Subject = createMessageDto.Subject,
+                Status = false, //okunmadı               
+            });
+            return Ok("Mesaj başarılı bir şekilde gönderildi");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteMessage(int id)
+        {
+            var value = _messageService.TGetByID(id);
+            _messageService.TDelete(value);
+            return Ok("Mesaj Silindi");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateMessage(UpdateMessageDto updateMessageDto)
+        {
+            _messageService.TUpdate(new Message
+            {
+                MessageID = updateMessageDto.MessageID,
+                MessageContent = updateMessageDto.MessageContent,
+                MessageSendDate = updateMessageDto.MessageSendDate,
+                Mail = updateMessageDto.Mail,
+                NameSurname=updateMessageDto.NameSurname,
+                Phone = updateMessageDto.Phone,
+                Status = false,
+                Subject = updateMessageDto.Subject,
+            });
+            return Ok("Mesaj bilgisi güncellendi");
+        }
+
+        [HttpGet("GetMessage")]
+        public IActionResult GetMessage(int id)
+        {
+            var value = _messageService.TGetByID(id);
+            return Ok(value);
+        }
+    }
+}
