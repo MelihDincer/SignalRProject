0803cc16ca709168535a9826ba125e7bd774600c
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-13 23:00:36 +0300

    Message Dtoları eklendi.

---
+++ SignalR.DtoLayer/MessageDto/UpdateMessageDto.cs
@@ -0,0 +1,14 @@
+﻿namespace SignalR.DtoLayer.MessageDto
+{
+    public class UpdateMessageDto
+    {
+        public int MessageID { get; set; }
+        public string NameSurname { get; set; }
+        public string Mail { get; set; }
+        public string Phone { get; set; }
+        public string Subject { get; set; }
+        public string MessageContent { get; set; }
+        public DateTime MessageSendDate { get; set; }
+        public bool Status { get; set; }
+    }
+}
