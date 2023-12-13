42c9f54e4eee05a4b4102f9c0e93a6feb7d8c8ee
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-13 23:00:47 +0300

    Slider Dtoları eklendi.

---
+++ SignalR.DtoLayer/SliderDto/CreateSliderDto.cs
@@ -0,0 +1,12 @@
+﻿namespace SignalR.DtoLayer.SliderDto
+{
+    public class CreateSliderDto
+    {
+        public string Title1 { get; set; }
+        public string Description1 { get; set; }
+        public string Title2 { get; set; }
+        public string Description2 { get; set; }
+        public string Title3 { get; set; }
+        public string Description3 { get; set; }
+    }
+}
