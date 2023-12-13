1aeba20ca863752b7c544f4e3d27bf8b89b97874
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-13 23:08:47 +0300

    SliderMapping eklendi.

---
+++ SignalRApi/Mapping/SliderMapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using SignalR.DtoLayer.SliderDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Mapping
+{
+    public class SliderMapping : Profile
+    {
+        public SliderMapping()
+        {
+            CreateMap<Slider, ResultSliderDto>().ReverseMap(); //Slider sınıfı ile ResultSliderDto sınıfını eşleştir. ReverseMap metodu ile hem Slider'ı ResultSliderDto ile, hem de ResultSliderDto'yu Slider ile eşleştirdik.
+            CreateMap<Slider, CreateSliderDto>().ReverseMap();
+            CreateMap<Slider, UpdateSliderDto>().ReverseMap();
+            CreateMap<Slider, GetSliderDto>().ReverseMap();
+        }
+    }
+}
