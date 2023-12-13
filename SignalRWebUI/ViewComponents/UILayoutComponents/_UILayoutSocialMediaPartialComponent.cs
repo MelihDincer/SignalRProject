9ec22370be48b70bdf348268cd414bcee9c12bdd
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-13 23:19:28 +0300

    SocialMedia ikonlarının footerda listelenmesi ve linklerinin verilmesi gibi işlemlerin dinamik olarak yapılması için viewcomponent oluşturuldu.

---
+++ SignalRWebUI/ViewComponents/UILayoutComponents/_UILayoutSocialMediaPartialComponent.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.SocialMedioDtos;
+
+namespace SignalRWebUI.ViewComponents.UILayoutComponents
+{
+    public class _UILayoutSocialMediaPartialComponent : ViewComponent
+    {
+        private readonly IHttpClientFactory _htppClientFactory;
+
+        public _UILayoutSocialMediaPartialComponent(IHttpClientFactory htppClientFactory)
+        {
+            _htppClientFactory = htppClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _htppClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7029/api/SocialMedia");
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultSocialMediaDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
