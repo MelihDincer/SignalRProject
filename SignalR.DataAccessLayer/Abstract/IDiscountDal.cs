c10dc40e4f95d57f98d05a55e7bb1b5b98ccd65f
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-13 22:40:01 +0300

    IDiscountDal'da entitye özgü metotların imzalarının yazılması.

--- SignalR.DataAccessLayer/Abstract/IDiscountDal.cs
+++ SignalR.DataAccessLayer/Abstract/IDiscountDal.cs
@@ -4,5 +4,9 @@ namespace SignalR.DataAccessLayer.Abstract
 {
     public interface IDiscountDal : IGenericDal<Discount>
     {
+        void ChangeStatusToTrue(int id);
+        void ChangeStatusToFalse(int id);
+
+        List<Discount> GetListByStatusTrue();
     }
 }
