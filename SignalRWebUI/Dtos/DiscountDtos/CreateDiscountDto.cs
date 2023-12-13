7944544d681feb4408e350e327e7fcb92b28d6b4
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-13 22:42:57 +0300

    Discount tablosunda yeni eklenen "Durum" propertysi için Dtoların revize edilmesi.

--- SignalRWebUI/Dtos/DiscountDtos/CreateDiscountDto.cs
+++ SignalRWebUI/Dtos/DiscountDtos/CreateDiscountDto.cs
@@ -6,5 +6,6 @@
         public string Amount { get; set; } //İndirim oranı
         public string Description { get; set; }
         public string ImageUrl { get; set; }
+        public bool Status { get; set; }
     }
 }
