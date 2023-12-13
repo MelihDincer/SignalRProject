cdd1224d981b108cac05e5a2bcaf3f6eac5ccc62
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-13 23:18:28 +0300

    Yanlış istek gönderilmesinden dolayı karşılaşılan hata düzeltildi.

--- SignalRWebUI/ViewComponents/DefaultComponents/_DefaultSliderPartialComponent.cs
+++ SignalRWebUI/ViewComponents/DefaultComponents/_DefaultSliderPartialComponent.cs
@@ -16,7 +16,7 @@ namespace SignalRWebUI.ViewComponents.DefaultComponents
         public async Task<IViewComponentResult> InvokeAsync()
         {
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:7029/api/Sliders");
+            var responseMessage = await client.GetAsync("https://localhost:7029/api/Slider");
             var jsonData = await responseMessage.Content.ReadAsStringAsync();
             var values = JsonConvert.DeserializeObject<List<ResultSliderDto>>(jsonData);
             return View(values);
