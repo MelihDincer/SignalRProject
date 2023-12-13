17e0ca6b0eacddb74c90d2013d0a242dd5bc07d2
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-13 22:38:26 +0300

    IDiscountService'da entitye özgü metotların imzalarının yazılması.

--- SignalR.BusinessLayer/Abstract/IDiscountService.cs
+++ SignalR.BusinessLayer/Abstract/IDiscountService.cs
@@ -4,5 +4,8 @@ namespace SignalR.BusinessLayer.Abstract
 {
     public interface IDiscountService : IGenericService<Discount>
     {
+        void TChangeStatusToTrue(int id);
+        void TChangeStatusToFalse(int id);
+        List<Discount> TGetListByStatusTrue();
     }
 }
