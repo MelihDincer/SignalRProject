e71708e2540b4d0bcecbbbf3a880607fefedcb3e
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-14 20:56:11 +0300

    SettingController oluşturuldu.

---
+++ SignalRWebUI/Controllers/SettingController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.EntityLayer.Entities;
+using SignalRWebUI.Dtos.IdentityDtos;
+
+namespace SignalRWebUI.Controllers
+{
+    public class SettingController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public SettingController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name); //Kullanıcı adı ile giriş yapıldığı için bu metot kullanıldı.
+            var userInformation = new UserEditDto
+            {
+                Username = user.UserName,
+                Name = user.Name,
+                Surname = user.Surname,
+                Mail = user.Email
+            };
+            return View(userInformation);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(UserEditDto userEditDto)
+        {
+            if(userEditDto.Password == userEditDto.ConfirmPassword)
+            {
+                var user = await _userManager.FindByNameAsync(User.Identity.Name);
+                user.Name = userEditDto.Name;
+                user.Surname = userEditDto.Surname;
+                user.Email = userEditDto.Mail;
+                user.UserName = userEditDto.Username;
+                user.PasswordHash = _userManager.PasswordHasher.HashPassword(user, userEditDto.Password); //user'dan gelen veriye göre userEditDto daki password bilgisini kullan
+                await _userManager.UpdateAsync(user);
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+        }
+    }
+}
