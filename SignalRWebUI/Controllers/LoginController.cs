381ad1df87d6c4389cb774d1118385b4a08a6987
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-14 20:53:54 +0300

    LogOut işleminin yapılması.

--- SignalRWebUI/Controllers/LoginController.cs
+++ SignalRWebUI/Controllers/LoginController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using SignalR.EntityLayer.Entities;
 using SignalRWebUI.Dtos.IdentityDtos;
 
 namespace SignalRWebUI.Controllers
 {
+	[AllowAnonymous]
 	public class LoginController : Controller
 	{
 		private readonly SignInManager<AppUser> _signInManager;
@@ -30,5 +32,12 @@ namespace SignalRWebUI.Controllers
 			}
 			return View();
 		}
-	}
+
+		public async Task<IActionResult> LogOut()
+		{
+			await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Login");
+
+        }
+    }
 }
