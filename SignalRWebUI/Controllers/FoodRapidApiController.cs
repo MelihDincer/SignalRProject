43a270c79f2c90ddf67d6785aa2eb73335ba635c
Melih DİNÇER <m_dincer41@hotmail.com>
2025-12-07 15:27:38 +0300

    Tasty Rapid API sayfası eklendi.

---
+++ SignalRWebUI/Controllers/FoodRapidApiController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.RapidApiDtos;
+using System.Threading.Tasks;
+
+namespace SignalRWebUI.Controllers
+{
+    public class FoodRapidApiController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public FoodRapidApiController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            
+            var client = _httpClientFactory.CreateClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://tasty.p.rapidapi.com/recipes/list?from=0&size=60&tags=under_30_minutes"),
+                Headers =
+    {
+        { "x-rapidapi-key", "4989b69e0bmsh169542d11e74854p1241b2jsnf9e27d1a6ff4" },
+        { "x-rapidapi-host", "tasty.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var root = JsonConvert.DeserializeObject<RootTastyApi>(body);
+                var values = root.Results;
+                return View(values);
+            }
+        }
+    }
+}
---
+++ SignalRWebUI/Dtos/RapidApiDtos/ResultTastyApiDto.cs
@@ -0,0 +1,14 @@
+﻿namespace SignalRWebUI.Dtos.RapidApiDtos
+{
+    public class RootTastyApi
+    {
+        public List<ResultTastyApiDto> Results { get; set; }
+    }
+    public class ResultTastyApiDto
+    {
+        public string name { get; set; }
+        public string video_url { get; set; }
+        public int total_time_minutes { get; set; }
+        public string thumbnail_url { get; set; }
+    }
+}
