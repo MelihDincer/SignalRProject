f1bb3b21abe117e04a47bcd95b0c6ce75e05a9d6
Melih DİNÇER <m_dincer41@hotmail.com>
2025-12-10 23:45:22 +0300

    Kurstaki son revizeler yapıldı ve yapılacaklar not alındı. İlerleyen zamanlarda revizeler gerçekleştirilecek.

--- SignalRWebUI/Controllers/BasketController.cs
+++ SignalRWebUI/Controllers/BasketController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using SignalR.DtoLayer.BasketDto;
 using SignalRWebUI.Dtos.BasketDtos;
 using System.Text;
 
@@ -14,27 +15,36 @@ namespace SignalRWebUI.Controllers
             _httpClientFactory = httpClientFactory;
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int id)
         {
+            TempData["id"] = id;
             var client = _httpClientFactory.CreateClient();
             //Burada id parametresi MenuTableID değerine karşılık gelmektedir.
-            var responseMessage = await client.GetAsync("https://localhost:7029/api/Baskets/BasketListByMenuTableWithProductName?id=4");
-            if(responseMessage.IsSuccessStatusCode)
+            var responseMessage = await client.GetAsync($"https://localhost:7029/api/Baskets/BasketListByMenuTableWithProductName?id={id}");
+            if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
                 var values = JsonConvert.DeserializeObject<List<ResultBasketDto>>(jsonData);
                 return View(values);
             }
             return View();
-        }    
-        
+        }
+
         public async Task<IActionResult> DeleteBasket(int id)
         {
+            int menutableId = int.Parse(TempData["id"].ToString());
             var client = _httpClientFactory.CreateClient();
             var responseMessage = await client.DeleteAsync($"https://localhost:7029/api/Baskets?id={id}");
-            if(responseMessage.IsSuccessStatusCode)
+            if (responseMessage.IsSuccessStatusCode)
             {
-                return RedirectToAction("Index");
+                var responseMessage2 = await client.GetAsync($"https://localhost:7029/api/Baskets/BasketListByMenuTableWithProductName?id={menutableId}");
+                var jsonData2 = await responseMessage2.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBasketDto>>(jsonData2);
+                if (values.Count == 0)
+                {
+                    await client.GetAsync($"https://localhost:7029/api/MenuTables/ChangeMenuTableStatusFalse?id={menutableId}");
+                }
+                return RedirectToAction("Index", new { id = menutableId });
             }
             return NoContent();
         }
